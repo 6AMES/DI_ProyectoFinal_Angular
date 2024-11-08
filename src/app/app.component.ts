85673f21f7fd3f9f4785f4cd71c032f82ebf21fe
Hernani <victor.heralb@gmail.com>
2024-11-08 13:45:01 +0100

    0.002
    Header terminado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { HombreComponent } from './hombre/hombre.component';
+import { MujerComponent } from './mujer/mujer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+            HeaderComponent, 
+            HomeComponent, 
+            FooterComponent,
+            HombreComponent,
+            MujerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
